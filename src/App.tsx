6e86002c7c991bf7190f46d4674da87f00746554
Nick <nickwentworth123@gmail.com>
2024-04-23 16:40:01 -0400

    setup empty project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+export default function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <button onClick={() => setCount((c) => c + 1)}>Count: {count}</button>
+    );
+}
