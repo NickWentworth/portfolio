8630b5b61b7dc099096c6fc50dea9c3e7824f1c7
Nick <nickwentworth123@gmail.com>
2024-04-29 18:09:09 -0400

    properly added font

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
 import App from './App.tsx';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import './global.css';
+
+const theme = extendTheme({
+    fonts: {
+        heading: "'Inter', sans-serif",
+        body: "'Inter', sans-serif",
+    },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <App />
         </ChakraProvider>
     </React.StrictMode>
