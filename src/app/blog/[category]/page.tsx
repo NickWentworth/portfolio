490ae508e75be5ddf7a09a083dcfca7e7759ded2
Nick <nickwentworth123@gmail.com>
2024-06-04 23:19:43 -0400

    added post navigation in category page

--- src/app/blog/[category]/page.tsx
+++ src/app/blog/[category]/page.tsx
@@ -1,3 +1,5 @@
+import { Card } from '@/components/common';
+import Link from 'next/link';
 import { getCategoryMeta, getPostMeta, getPostText } from '@/lib/posts';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
@@ -9,12 +11,29 @@ type PageProps = {
 
 export default (props: PageProps) => {
     const text = getPostText(props.params.category);
+    const posts = getPostMeta(props.params.category);
 
     if (text === undefined) {
         return <p>Error: file not found</p>;
     }
 
-    return <MDXRemote source={text} />;
+    return (
+        <div className='flex flex-col gap-4'>
+            <MDXRemote source={text} />
+
+            <hr />
+
+            <div className='grid grid-cols-2 gap-4'>
+                {posts.map((post) => (
+                    <Link key={post.title} href={post.href}>
+                        <Card>
+                            <h5>{post.title}</h5>
+                        </Card>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export function generateStaticParams() {
